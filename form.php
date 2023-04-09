908e93dfe7420771b68f09585c9107140ab43512
kasibasulthana <128136291+kasibasulthana@users.noreply.github.com>
2023-04-09 16:01:51 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
-echo "helloooo muni";
+
 
 $regno=$_POST['regno'];
 $sname=$_POST['sname'];
@@ -24,8 +24,8 @@ if($conn->connect_errno){
 $sql="INSERT INTO student(regno,sname,dob,passwd,email,phno,gender,chfile,saddress)VALUES('$regno','$sname','$dob','$passwd','$email','$phone','$gender','$file','$address')";
 $query=mysqli_query($conn,$sql);
 if($query){
-    echo "<center><p>Submitted</p></center>";
-    echo '<center><button  ><a href="index.html" >Go to home</a></button></center>';
+    echo "<center><p style='font-size:20px; font-family:arial;'>Submitted</p></center>";
+    echo '<center><button style="padding:14px; background-color:black; border-radius 6px " ><a href="index.html" style="text-decoration:none; color:white;">Go to home</a></button></center>';
 }
 else{
     echo "Unable to submit the form",$conn->error;
